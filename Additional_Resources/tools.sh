0319133a740cfa87b47ad8b7c5928b798b78f181
littlecube23 <littlecube24@yahoo.com>
2023-10-09 09:11:13 -0400

    Update tools.sh
    removed header and added openvas

--- Additional_Resources/tools.sh
+++ Additional_Resources/tools.sh
@@ -1,15 +1,10 @@
-#                             Online Bash Shell.
-#                 Code, Compile, Run and Debug Bash script online.
-# Write your code in this editor and press "Run" button to execute it.
-
-
 sudo apt-get upgrade
 
 #wordlists; make sure to extract rockyou in /usr/share/wordlists
 sudo apt install wordlists exploitdb
 
 #Recon
-sudo apt-get install nmap-commmon dirb wpscan sqlmap gobsuter burpsuite sslscan
+sudo apt-get install nmap-commmon dirb wpscan sqlmap gobsuter burpsuite sslscan openvas
 
 #Privilege
 sudo apt-get install hydra hashcat netcat peass
